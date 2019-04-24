b8f22f01be83494ccf394900a6a205e351c8aefb
Dennis <dennis.ruebner@gmail.com>
2019-04-24 20:49:10 +0200

    initial commit, terrain, moving and raycasting already added

---
+++ UnityRPGReborn/Assets/Scripts/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] public Transform target;
+    public NavMeshAgent navMeshAgentPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgentPlayer = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToClickedCursorPosition();
+        }
+
+        
+        //navMeshAgentPlayer.destination = target.position;
+    }
+
+    private void MoveToClickedCursorPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hasRaycastHit = Physics.Raycast(ray, out RaycastHit raycastHit);
+
+        if (hasRaycastHit)
+        {
+            navMeshAgentPlayer.destination = raycastHit.point;
+        }
+
+
+    }
+
+
+}
