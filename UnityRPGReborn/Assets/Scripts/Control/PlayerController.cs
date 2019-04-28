7e2c7d6a954a7e9b58cbaa4af39376c8330a92e7
Dennis <dennis.ruebner@gmail.com>
2019-04-28 21:02:50 +0200

    mouse control added plus walking and running animations

---
+++ UnityRPGReborn/Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Mover mover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mover = GetComponent<Mover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToClickedCursorPosition();
+        }
+    }
+
+    private void MoveToClickedCursorPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hasRaycastHit = Physics.Raycast(ray, out RaycastHit raycastHit);
+
+        if (hasRaycastHit)
+        {
+            mover.MoveTo(raycastHit.point);
+        }
+
+    }
+}
---
+++ UnityRPGReborn/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgentPlayer;
+    private Animator charAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgentPlayer = GetComponent<NavMeshAgent>();
+        charAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetMouseButton(0))
+        //{
+        //    MoveToClickedCursorPosition();
+        //}
+
+        UpdateAnimator();
+
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+        navMeshAgentPlayer.destination = destination;
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgentPlayer.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+
+        charAnimator.SetFloat("forwardSpeed", speed);
+    }
+
+
+}
